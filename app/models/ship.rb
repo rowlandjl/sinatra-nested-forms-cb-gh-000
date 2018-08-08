b8b7cbd93e08be0328f995b53c6aa8059a20968d
Jessica Rowland <rowlandjl@vcu.edu>
2018-08-08 14:10:08 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+
+  attr_accessor :name, :type, :booty
+
+  SHIPS = []
+
+  def initialize(details)
+    details.each { |key, value| self.send("#{key}=", value)}
+    self.class.all << self
+  end
+
+  def self.all
+    SHIPS
+  end
+
+  def self.clear
+    SHIPS.clear
+  end
+  
 end
