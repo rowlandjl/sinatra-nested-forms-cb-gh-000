4cd858cd1da4d21611b91a1b9dd7f14dc4432a3a
Jessica Rowland <rowlandjl@vcu.edu>
2018-08-08 14:04:52 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,19 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+
+  PIRATES = []
+
+  def initialize(details)
+    self.name = details[:name]
+    self.weight = details[:weight]
+    self.height = details[:height]
+
+    self.class.all << self
+  end
+
+  def self.all
+    PIRATES
+  end
+  
 end
