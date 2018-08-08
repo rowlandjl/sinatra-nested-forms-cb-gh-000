6a5fbd73d77c8a41a948389fbbb182a2545db9d1
Jessica Rowland <rowlandjl@vcu.edu>
2018-08-08 14:06:22 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,20 +1,20 @@
 class Ship
 
-  attr_accessor :name, :type, :booty 
+  attr_accessor :name, :type, :booty
 
   SHIPS = []
 
   def initialize(details)
     details.each { |key, value| self.send("#{key}=", value)}
-    self.class.all << self 
-  end 
+    self.class.all << self
+  end
 
-  def self.all 
-    SHIPS 
-  end 
+  def self.all
+    SHIPS
+  end
 
-  def self.clear 
-    SHIPS.clear 
-  end 
+  def self.clear
+    SHIPS.clear
+  end
   
 end
